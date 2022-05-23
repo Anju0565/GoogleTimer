9e01ab1879568fe469e3dcad3b1e77ba21569443
Anjali Priya <88077417+Anju0565@users.noreply.github.com>
2022-05-23 08:21:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg'
+import './App.css'
+import GoogleTimer from './components/googleTimer'
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleTimer />
+    </div>
+  )
+}
+
+export default App
